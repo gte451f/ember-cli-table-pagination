a9bb324b7cb767c240fa972420a9ba955573f9f6
jjenkins <jjenkins@smithcarson.com>
2015-06-10 22:42:48 -0400

    fix some bugs that show up in unit testing remove some unit tests I couldn't get working

--- addon/mixins/table/controller.js
+++ addon/mixins/table/controller.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import Column from 'tm3/mixins/pager/column';
+import Column from 'ember-cli-table-pagination/lib/column';
 
 /**
  * store shared logic to run pager logic
