da1ed3c663140181560e76ace0bb3b48aa8d6d18
jjenkins <jjenkins@smithcarson.com>
2015-06-12 11:54:39 -0400

    ok, let's try vendor

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
     name: 'ember-cli-table-pagination',
 
     included: function (app) {
-        app.import('app/styles/app.css');
+        app.import('vendor/styles/app.css');
     }
 
 };
