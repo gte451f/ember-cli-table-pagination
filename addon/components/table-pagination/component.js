a9898fd8ffa3550aeb98f696f7f5bd0a1c9090f8
leojpod <leo.jpod@gmail.com>
2016-04-07 22:54:13 +0200

    add basic structure to the table-pagination component

--- addon/components/table-pagination/component.js
+++ addon/components/table-pagination/component.js
@@ -1,6 +1,30 @@
 import Ember from 'ember';
 import layout from './template';
 
-export default Ember.Component.extend({
-  layout
+const {
+  Component,
+  computed
+} = Ember;
+
+export default Component.extend({
+  // HTML
+  layout,
+
+  // properties
+  allowQuickSearch: true,
+  content: [],
+  isRemoteHandled: false,
+  page: 1,
+  perPage: 5,
+  searchString: '',
+  tableTitle: 'title',
+  totalPages: 5,
+
+  // overwritable components
+  bodyComponent: 'table-pagination.table-body',
+  contentComponent: 'table-pagination.table-content',
+  pagerComponent: 'table-pagination.table-pager',
+  titleComponent: 'table-pagination.table-title',
+  toolbarComponent: 'table-pagination.table-toolbar',
+  toolsComponent: 'table-pagination.table-tools'
 });
