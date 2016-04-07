251bd840af5f1d03ce45ad4106c84845624cd92a
leojpod <leo.jpod@gmail.com>
2016-04-07 22:55:10 +0200

    add simple pagination feature and basic filtering option to the table-pagination component

--- addon/components/table-pagination/component.js
+++ addon/components/table-pagination/component.js
@@ -20,6 +20,44 @@ export default Component.extend({
   tableTitle: 'title',
   totalPages: 5,
 
+  // computed
+  filteredContent: computed('content', 'searchString', 'isRemoteHandled', function() {
+    let content = this.get('content');
+    let isRemoteHandled = this.get('isRemoteHandled');
+    if (isRemoteHandled) {
+      return content;
+    } else {
+      let searchString = this.get('searchString');
+      return content.filter(function(item) {
+        Ember.Logger.debug('item -> ', item);
+        Ember.Logger.debug(arguments);
+        let pattern = new RegExp(searchString, 'i');
+        let found = false;
+        item.eachAttribute(function(name, meta) {
+          if (['number', 'string', 'date'].contains(meta.type)) {
+            found = found || pattern.test(item.get(name));
+          }
+        });
+        Ember.Logger.debug('is item %s matching %s ? -> %s', item.get('idNum'), searchString, found);
+        return found;
+      });
+    }
+  }),
+  currentContent: computed('filteredContent', 'page', 'perPage', 'isRemoteHandled', function() {
+    let content = this.get('filteredContent');
+    let isRemoteHandled = this.get('isRemoteHandled');
+    if (isRemoteHandled) {
+      return content;
+    } else {
+      let page = this.get('page');
+      let perPage = this.get('perPage');
+
+      // we are handling the data ourself so:
+      // we should display only the items on the current page:
+      // a.k.a. perPage items starting at perPageItems*page
+      return content.slice(perPage * (page - 1), perPage * (page - 1) + perPage);
+    }
+  }),
   // overwritable components
   bodyComponent: 'table-pagination.table-body',
   contentComponent: 'table-pagination.table-content',
