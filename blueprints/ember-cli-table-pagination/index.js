0813ec552b2bbf3e3b781b0a4ccb81d142a8adb1
leojpod <leo.jpod@gmail.com>
2016-04-23 16:44:15 +0200

    Revert "try changing the blueprint"
    This reverts commit 9f6815ed2ef27ef7898fe5f2f444d2b5af1e816a.

--- blueprints/ember-cli-table-pagination/index.js
+++ blueprints/ember-cli-table-pagination/index.js
@@ -1,6 +1,6 @@
 /*jshint node:true*/
 module.exports = {
-  description: 'install the addon'// ,
+  description: 'install the addon',
 
   // locals: function(options) {
   //   // Return custom template variables here.
@@ -12,9 +12,9 @@ module.exports = {
   // afterInstall: function(options) {
   //   // Perform extra work here.
   // }
-  // normalizeEntityName() {
-  //   // this prevents an error when the entityName is
-  //   // not specified (since that doesn't actually matter
-  //   // to us
-  // }
+  normalizeEntityName() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  }
 };
