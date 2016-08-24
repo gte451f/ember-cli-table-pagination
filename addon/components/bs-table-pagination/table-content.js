49f68554efb8ab6dec8346971b5a475b824fc847
Jim <jim.jenkins.iv@gmail.com>
2016-08-24 15:22:50 -0400

    TM-183 - revamp some pagination properties

--- addon/components/bs-table-pagination/table-content.js
+++ addon/components/bs-table-pagination/table-content.js
@@ -11,9 +11,10 @@ export default Ember.Component.extend({
 
   showFilter: false,
 
-  columns: reads('contentParams.columns'),
-  sorting: reads('contentParams.sorting'),
-  sortingDirection: reads('contentParams.sortDirection'),
-  loading: reads('contentParams.loading'),
-  numberOfRecords: reads('contentParams.numberOfRecords')
+  // these should be defined on the main pagination component
+  // columns: reads('contentParams.columns'),
+  // sorting: reads('contentParams.sorting'),
+  // sortingDirection: reads('contentParams.sortDirection'),
+  // loading: reads('contentParams.loading'),
+  // numberOfRecords: reads('contentParams.numberOfRecords')
 });
