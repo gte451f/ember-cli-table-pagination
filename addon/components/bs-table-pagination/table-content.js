c7090467058f050777e52a26d695451da4c6a7ad
Jim <jim.jenkins.iv@gmail.com>
2016-09-23 16:59:46 -0400

    merge in changes

--- addon/components/bs-table-pagination/table-content.js
+++ addon/components/bs-table-pagination/table-content.js
@@ -1,9 +1,6 @@
 import Ember from 'ember';
 import layout from '../../templates/components/bs-table-pagination/table-content';
 
-const {computed} = Ember;
-const {reads} = computed;
-
 export default Ember.Component.extend({
   layout,
   classNames: ['ember-cli-table-content'],
