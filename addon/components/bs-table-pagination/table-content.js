1fa483cd2acbb18300ee5ff17ad4e2bbfd12f8ec
leojpod <leo.jpod@gmail.com>
2016-10-17 21:21:53 +0200

    allowing table's content to scroll part I

--- addon/components/bs-table-pagination/table-content.js
+++ addon/components/bs-table-pagination/table-content.js
@@ -1,10 +1,48 @@
 import Ember from 'ember';
 import layout from '../../templates/components/bs-table-pagination/table-content';
+import ResizeAware from 'ember-resize/mixins/resize-aware';
 
-export default Ember.Component.extend({
+const { Component, computed, on } = Ember;
+const { reads } = computed;
+
+export default Component.extend(ResizeAware, {
   layout,
+  resizeHeightSensitive: true,
+  resizeWidthSensitive: false,
+
   classNames: ['ember-cli-table-content'],
   // tagName: '',
 
-  showFilter: false
+  showFilter: false,
+  scrollMode: reads('contentParams.scrollMode'),
+  mainScroll: reads('contentParams.mainScroll'),
+  bodyHeight: reads('contentParams.bodyHeight'),
+
+  onInit: on('didInsertElement', function () {
+    console.log('on Init !');
+    if (this.get('mainScroll')) {
+      this.adjustBodyHeight();
+    }
+  }),
+
+  didResize(width, height) {
+    console.log(`Resized! ${width}x${height}`);
+    if (this.get('mainScroll')) {
+      this.adjustBodyHeight();
+    }
+  },
+
+  adjustBodyHeight() {
+    let documentHeight = document.body.clientHeight;
+    console.log('documentHeight -> ', documentHeight);
+    let above = this.$('tbody').offset().top;
+    console.log('above -> ', above);
+    let height = this.$('tbody').outerHeight();
+    let under = documentHeight - (above + height);
+    console.log('under -> ', under);
+
+    let bodyHeight = `${window.innerHeight - above - under}px`;
+    console.log('resizing to -> ', bodyHeight);
+    this.set('bodyHeight', bodyHeight);
+  }
 });
