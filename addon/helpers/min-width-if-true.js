53066eea84f0e22114641bccacfdb420edb32639
Marcelo Andrade R <marcelo.andrade.r@gmail.com>
2016-11-18 10:29:34 -0500

    TM-294 - fix deprecation with style binding

---
+++ addon/helpers/min-width-if-true.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export function minWidthIfTrue(params/*, hash*/) {
+  let isTrue = params[0];
+  let percentage = parseFloat(params[1]);
+  if (isTrue) {
+    return Ember.String.htmlSafe('min-width: ' + percentage + '%');
+  }
+  return '';
+}
+
+export default Ember.Helper.helper(minWidthIfTrue);
