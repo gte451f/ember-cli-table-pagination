81f2e4a401a112e48ec661842356e6eabb37f7fc
Marcelo Andrade R <marcelo.andrade.r@gmail.com>
2017-01-09 13:34:33 -0500

    TM-535 - show action buttons

--- addon/components/light-table-pagination.js
+++ addon/components/light-table-pagination.js
@@ -4,7 +4,8 @@ import TablePagination from './table-pagination';
 import Table from 'ember-light-table';
 
 const {
-  computed
+  computed,
+  isEmpty
 } = Ember;
 
 export default TablePagination.extend({
@@ -15,7 +16,7 @@ export default TablePagination.extend({
 
   // override the components:
   bodyComponent: 'bs-table-pagination.table-body',
-  contentComponent: 'bs-table-pagination.table-content',
+  contentComponent: 'bs-table-pagination.table-content', // NOT USED anymore
   footerComponent: 'bs-table-pagination.table-footer',
   pagerComponent: 'bs-table-pagination.table-pager',
   titleComponent: 'bs-table-pagination.table-title',
@@ -25,16 +26,31 @@ export default TablePagination.extend({
 
   /** light table columns derived from the columns property*/
   ltColumns: computed('columns', function() {
-    return this.get('columns').map((column) => {
+    return [ {
+        label: 'Actions',
+        sortable: false,
+        width: '50px',
+        cellComponent: 'light-table-pagination.table-actions'
+      } ].pushObjects(
+      this.get('columns').map((column) => {
       return {
         label: column.get('displayName'),
         valuePath: column.get('fieldName'),
         sortable: true
       };
-    });
+    }));
   }),
-  table: computed('content', 'ltColumns', function() {
-    return new Table(this.get('ltColumns'), this.get('content'));
+  table: computed('ltColumns', 'content', function() {
+    let t = new Table(this.get('ltColumns'), this.get('content'));
+
+    let tpColumn = this.get('columns').findBy('apiInteractionName', this.get('sortProperty'));
+    let sortColumn = t.get('allColumns').findBy('valuePath', tpColumn.get('fieldName'));
+
+    if (sortColumn) {
+      sortColumn.set('sorted', true);
+      sortColumn.set('ascending', this.get('sortDirection') === 'asc');
+    }
+    return t;
   }),
 
   actions: {
@@ -42,7 +58,12 @@ export default TablePagination.extend({
       if (column.sorted) {
         /** get the table pagination column */
         let tpColumn = this.get('columns').findBy('fieldName', column.get('valuePath'));
-        this.sendAction('changeSort', tpColumn.get('apiInteractionName'), column.get('ascending') ? '' : '-');
+        this.sendAction('changeSort', tpColumn.get('apiInteractionName'), column.get('ascending') ? 'asc' : 'desc');
+      }
+    },
+    onColumnDoubleClick(column) {
+      if (column.sorted) {
+        alert('Here we can put a modal to filter');
       }
     }
   }
---
+++ addon/components/light-table-pagination/table-actions.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import layout from '../../templates/components/light-table-pagination/table-actions';
+
+export default Ember.Component.extend({
+  layout,
+  tagName: ''
+});
+
