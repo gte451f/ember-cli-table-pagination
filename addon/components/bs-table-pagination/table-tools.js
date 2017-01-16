721fd8f528db251e0536df1bb361e08385551488
Marcelo Andrade R <marcelo.andrade.r@gmail.com>
2017-01-16 10:52:17 -0500

    TM-535 - show all records button

--- addon/components/bs-table-pagination/table-tools.js
+++ addon/components/bs-table-pagination/table-tools.js
@@ -6,5 +6,20 @@ export default Ember.Component.extend({
   classNames: ['col-md-7'],
 
   // properties
-  perPageOptions: [50, 100, 250]
+  perPageOptions: [50, 100, 250],
+
+  showAll: false,
+
+  actions: {
+    showAllRecords() {
+      this.set('showAll', true);
+      this.attrs.changePerPage(100000000); // 100 million records
+      this.attrs.refresh();
+    },
+    showByPageRecords() {
+      this.set('showAll', false);
+      this.attrs.changePerPage(this.get('perPageOptions.firstObject'));
+      this.attrs.refresh();
+    }
+  }
 });
