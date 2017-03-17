a880f16e61eca58d6ed6e63339e3314b7eeacbf0
John King <jking6884@yahoo.com>
2017-03-17 17:44:16 -0400

    JSI-130

--- addon/components/light-table/columns/base-with-filter.js
+++ addon/components/light-table/columns/base-with-filter.js
@@ -2,5 +2,6 @@ import layout from '../../../templates/components/light-table/columns/base-with-
 import ColumnsBase from 'ember-light-table/components/columns/base';
 
 export default ColumnsBase.extend({
-  layout
+  layout,
+  classNames: ['lt-column', 'nowrap'],
 });
