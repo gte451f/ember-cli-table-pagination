c77330416acaf6761d58f6385c76fbc10dc084f8
Bogdan <sirbrisc@gmail.com>
2017-09-19 13:11:41 +0300

    Upgrade to Ember 2.15

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -2,11 +2,12 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
-  this.route('old', { restNamespace: true });
+  this.route('old', { resetNamespace: true });
   this.route('item', { path: '/item/:item_id' });
   this.route('bootstrap');
   this.route('bs-remote');
