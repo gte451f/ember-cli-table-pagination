2dab620374ed4a6ad106cba52c92ff946c6305c9
Bogdan <sirbrisc@gmail.com>
2018-10-15 17:38:14 +0300

    DEV-58 - Load table data using ember-concurrency and table settings. Filters changes

---
+++ addon/mixins/table-data/controller.js
@@ -0,0 +1,336 @@
+import { on } from '@ember/object/evented'
+import Mixin from '@ember/object/mixin'
+import EmberObject, { computed, observer } from '@ember/object'
+import { isPresent, isEmpty, typeOf } from '@ember/utils'
+import { alias } from '@ember/object/computed'
+import { merge } from '@ember/polyfills'
+import { underscore } from '@ember/string'
+import { task, timeout } from 'ember-concurrency'
+import Column from './column'
+
+/**
+ * store shared logic to manage table data
+ */
+export default Mixin.create({
+  // used to store the table data (rows, old model)
+  tableData: null,
+  // object with parameters used for table data loading
+  loadDataParams: null,
+
+  useTableSettings: true,
+  saveTableSettings: task(function * () {
+    this.set('currentControllerState', this.toTableSettingsState())
+    if (this.get('currentTableSetting')) {
+      if (this.get('currentTableSetting.state') !== JSON.stringify(this.get('currentControllerState'))) {
+        this.set('currentTableSetting.state', JSON.stringify(this.get('currentControllerState')))
+        yield this.get('currentTableSetting').save()
+      }
+    } else {
+      const tableSetting = this.store.createRecord('table-setting', {
+        route: this.get('routeKey'),
+        appName: this.get('appName'),
+        state: JSON.stringify(this.get('currentControllerState'))
+      })
+      yield tableSetting.save()
+      this.set('currentTableSetting', tableSetting)
+    }
+  }).restartable(),
+
+  loadTableSettings: task(function * () {
+    if (!this.get('loadedFromTableSettings')) {
+      // Only load from the api once
+      const ts = yield this.store.queryRecord('table-setting', {
+        route: this.get('routeKey'),
+        app_name: this.get('appName')
+      })
+      this.set('currentTableSetting', ts)
+      this.set('currentControllerState', null)
+      // comes from another route, check saved params in db
+      if (isPresent(ts)) {
+        this.set('currentControllerState', JSON.parse(ts.get('state')))
+      }
+      this.fromTableSettingsState(this.get('currentControllerState'))
+      this.updateColumnsFromTableSettingState(this.get('currentControllerState'))
+      this.set('loadedFromTableSettings', true)
+    }
+
+  }),
+
+  loadTableData: task(function * () {
+    // load table settings if they are active
+    const useTableSettings = this.get('useTableSettings')
+    if (useTableSettings) {
+      yield this.get('loadTableSettings').perform()
+    }
+    // gather params
+    const params = this.get('loadDataParams')
+    const allParams = this.getAllParams(params)
+
+    const tableData = []
+    let records = yield this.get('store').query(this.get('modelName'), allParams)
+    if (records) {
+      tableData.pushObjects(records.toArray())
+    }
+    // process table data
+    const processedTableData = this.processTableData(tableData)
+    // store the result in tableData
+    this.set('tableData', processedTableData)
+
+    // store properties on to controller
+    this.set('currentParams', allParams)
+    this.set('totalRecords', records.get('meta.total_record_count'))
+    let {sortField} = allParams
+    if (sortField) {
+      if (sortField.substring(0, 1) === '-') {
+        this.set('sortDirection', 'desc')
+        this.set('sortProperty', sortField.substring(1))
+      } else {
+        this.set('sortDirection', 'asc')
+        this.set('sortProperty', sortField)
+      }
+      this.set('sortField', sortField)
+    }
+    // save new table settings if they are active
+    if (useTableSettings) {
+      this.get('saveTableSettings').perform()
+    }
+  }).restartable(),
+
+  // function should be overwritten if the table data has to be processed before it is used in the table
+  processTableData (tableData) {
+    return tableData
+  },
+
+  getAllParams: function (params) {
+
+    const name = this.get('quickSearchField')
+    const value = this.get('quickSearch')
+    const queryWith = this.get('with')
+    const allParams = merge(params, {
+      page: this.get('page'),
+      perPage: this.get('perPage'),
+      sortField: this.get('sortField'),
+      with: queryWith
+    })
+
+    if (isPresent(name) && isPresent(value)) {
+      params[name] = '*' + value.trim() + '*'
+    }
+
+    // To pass already formatted params to the query
+    let extraParams = this.get('extraParams')
+    for (let fieldName in extraParams) {
+      params[underscore(fieldName)] = extraParams[fieldName]
+    }
+    // filter params should override the extra params ?
+
+    let filterParams = this.get('filterParams')
+    for (let fieldName in filterParams) {
+      let filterValue = filterParams[fieldName]
+      if (typeOf(filterValue) !== 'null' && isPresent(filterValue)) {
+        params[underscore(fieldName)] = '*' + filterValue + '*'
+      }
+    }
+    return allParams
+  },
+
+  columnFilterValueChanged (column) {
+    if (typeOf(column.get('filterValue')) !== 'undefined' && typeOf(column.get('filterValue')) !== 'null') {
+      this.send('applyFilter', column.get('apiInteractionName'), column.get('filterValue'))
+    }
+  },
+  configureFilterObservers () {
+    const controller = this
+    const filterParams = {}
+    const useTableSettings = this.get('useTableSettings')
+    this.get('columns').forEach(function (column, index) {
+      column.set('filterValueChanged', function (col) {return controller.columnFilterValueChanged(col)})
+      if (!useTableSettings) {
+        // const key = column.get('apiInteractionName')
+        // filterParams[key] = null
+      }
+    })
+    if (!useTableSettings) {
+      // this.set('filterParams', filterParams)
+    }
+    return null
+  },
+
+  // setup our query params including custom sortField value
+  queryParams: ['page', 'perPage', 'sortField', 'with'],
+
+  pageList: [
+    50, 100, 250
+  ],
+
+  totalPages: alias('tableData.totalPages'),
+
+  observingPerPage: observer('perPage', function () {
+    this.set('page', 1)
+  }),
+
+  sortOrder: computed('sortDirection', function () {
+    let sortDirection = this.get('sortDirection')
+    if (sortDirection === 'desc') {
+      return '-'
+    } else {
+      return ''
+    }
+  }),
+  with: '',
+
+  // disabled by default
+  // set these in the controller to enable and bind to a specific field
+  quickSearchField: null,
+  quickSearch: null,
+
+  page: 1,
+  perPage: 50,
+  totalRecords: null,
+
+  column: EmberObject.extend({
+    display: null,
+    field: null
+  }),
+
+  quickSearchChanged: observer('quickSearch', function () {
+    this.send('runQuickSearch')
+  }),
+
+  // load pager specific variables
+  columns: [
+    Column.create({'displayName': '#', 'fieldName': 'id'})
+  ],
+
+  additionalColumnsForFilter: [],
+
+  // bootstrap or adminlte specific classes
+  // color default|success|primary|warning|danger|info
+  box: 'default',
+  button: 'default',
+
+  // display the table title if there is one
+  tableTitle: 'set tableTitle in controller to NULL to hide otherwise set this to your table title',
+
+  // where should the default open action link-to?
+  linkPath: false,
+  editPath: false,
+
+  // not sure what this is
+  createPath: 'set createPath in the controller',
+
+  canLoadMore: false,
+
+  selectedRows: [],
+
+  /**
+   * @method
+   * @public
+   *
+   * allows serialize the controller state to be able to send it to storage
+   */
+  toTableSettingsState () {
+    // We need to return a json object
+    // that can be used to re-hydrate the controller
+    // after a cold refresh to use saved filters
+    // sorting, page, perPage etc that are not part of
+    // the queryParams
+
+    const mapColumn = (col) => {
+      return {
+        fieldName: col.get('fieldName'),
+        filterValue: col.get('filterValue'),
+        showFilter: col.get('showFilter'),
+        advFilterValue: col.get('advFilterValue'),
+        advFilterValue2: col.get('advFilterValue2'),
+        advFilterOperator: col.get('advFilterOperator')
+      }
+    }
+
+    return {
+      page: this.get('page'),
+      perPage: this.get('perPage'),
+      sortField: this.get('sortField'),
+      quickSearch: this.get('quickSearch'),
+      extraParams: this.get('extraParams'),
+      filterParams: this.get('filterParams'),
+      columns: this.get('columns').map(mapColumn),
+      additionalColumnsForFilter: this.get('additionalColumnsForFilter').map(mapColumn)
+    }
+  },
+
+  /**
+   * @method
+   * @public
+   *
+   * allows deserialize the controller state from a storage
+   */
+  fromTableSettingsState (state) {
+    if (isEmpty(state)) return
+    this.set('page', state.page)
+    this.set('perPage', state.perPage)
+    this.set('sortField', state.sortField)
+    this.set('quickSearch', state.quickSearch)
+    this.set('filterParams', state.filterParams)
+  },
+
+  updateColumnsFromTableSettingState (state) {
+    const updateColumn = function (list) {
+      return (col) => {
+        const tableColumn = list.findBy('fieldName', col.fieldName)
+        if (isPresent(tableColumn)) {
+          tableColumn.set('filterValue', col.filterValue)
+          tableColumn.set('showFilter', col.showFilter)
+          tableColumn.set('advFilterValue', col.advFilterValue)
+          tableColumn.set('advFilterValue2', col.advFilterValue2)
+          if (isPresent(col.advFilterOperator)) {
+            tableColumn.set('advFilterOperator', EmberObject.create(col.advFilterOperator))
+          }
+        }
+      }
+    }
+
+    if (state) {
+      state.columns.forEach(updateColumn(this.get('columns')))
+      state.additionalColumnsForFilter.forEach(updateColumn(this.get('additionalColumnsForFilter')))
+    }
+  },
+
+  actions: {
+    editFlag (row) {
+      this.set('isEditingFlagNote', true)
+      this.set('flaggingRecord', row.content)
+    },
+
+    saveFlag () {
+      let row = this.get('flaggingRecord')
+
+      let subject = this.store.peekRecord('subject', row.id)
+
+      subject.set('flagNote', row.flagNote)
+
+      if (isEmpty(row.get('flagNote'))) {
+        subject.set('flagStatus', 'unflagged')
+      } else {
+        subject.set('flagStatus', 'flagged')
+      }
+      subject.save().then(() => {
+        this.get('notify').success('Saved successfully')
+      }).then(() => {
+        if (isEmpty(row.get('flagNote'))) {
+          row.set('flagStatus', 'unflagged')
+        } else {
+          row.set('flagStatus', 'flagged')
+        }
+
+        this.set('isEditingFlagNote', false)
+        this.set('flaggingRecord', null)
+      })
+    },
+
+    cancelFlag () {
+      this.set('isEditingFlagNote', false)
+      this.set('flaggingRecord', null)
+    }
+  }
+})
---
+++ addon/mixins/table-data/route.js
@@ -0,0 +1,262 @@
+import { merge } from '@ember/polyfills'
+import { isPresent, typeOf } from '@ember/utils'
+import Mixin from '@ember/object/mixin'
+import { underscore } from '@ember/string'
+import RouteMixin from 'ember-cli-pagination/remote/route-mixin'
+import { task, timeout } from 'ember-concurrency'
+import Ember from 'ember'
+
+export default Mixin.create(RouteMixin, {
+  /**
+   * set the sortField property as a query param so it will show up in the url
+   * refreshModel affects route behavior when the value changes
+   */
+  queryParams: {
+    sortField: {
+      refreshModel: true
+    },
+    page: {
+      refreshModel: false
+    },
+    perPage: {
+      refreshModel: false
+    }
+  },
+
+  // store a reference to the route's model
+  // i couldn't figure out how to look this up
+  modelName: 'set modelName in the route',
+
+  // store a reference to the current route, since I don't know how to look this up
+  currentRoute: 'set currentRoute in the route',
+
+  currentParams: null,
+
+  routeRefresh () {
+    this.refresh()
+  },
+  refreshAfterFilters: task(function * () {
+    // debounce the filter to 600 ms
+    yield timeout(1000)
+    if (this.controller.get('page') !== 1) {
+      this.controller.set('page', 1)
+    }
+    this.refresh()
+  }).restartable(),
+
+  actions: {
+    // action called from the button at the right of the number per page list
+    refresh () {
+      this.refresh()
+    },
+
+    /**
+     * take in a sortField and store the new sortField in the controller
+     * also infer the correct sort order (ASC or DESC)
+     * works with query params
+     * follows json:api conventions
+     *
+     * @param field
+     */
+    sortField (field) {
+      field = underscore(field)
+      const sortField = this.controller.get('sortField')
+      const sortOrder = this.controller.get('sortOrder')
+
+      // sortField hasn't changed so we toggle sortOrder
+      // check for the descending and ascending versions
+      if (field === sortField || '-' + field === sortField) {
+        if (sortOrder === '-') {
+          this.controller.set('sortOrder', '')
+        } else {
+          this.controller.set('sortOrder', '-')
+        }
+      }
+      // always update the sortField..either the field changes or the order changes
+      this.controller.set('sortField', this.controller.get('sortOrder') + field)
+    },
+
+    /**
+     * sets a new param on the filterParams property
+     * of the controller and fetch results with that
+     *
+     * It always return to page 1, because if the table
+     * is in page 3 and set a filter with 1 result,
+     * that page is empty
+     */
+    applyFilter (fieldName, filterValue) {
+      let params = this.controller.get('filterParams')
+      if (params === undefined) {
+        // when useSettings is false, the params should be an empty object
+        params = {}
+      }
+      if ((params[fieldName] == null && filterValue === '') || params[fieldName] === filterValue) {
+        return
+      }
+      params[fieldName] = filterValue
+      this.controller.set('filterParams', params)
+
+      // do not trigger a table refresh when the columns filter value is set from table settings
+      if (this.controller.get('useTableSettings') && !this.controller.get('loadedFromTableSettings')) {
+        return
+      }
+      this.get('refreshAfterFilters').perform()
+    },
+
+    // take the supplied search field and ask the api to filter by it
+    runQuickSearch () {
+      // do not trigger a table refresh when the columns filter value is set from table settings
+      if (this.controller.get('useTableSettings') && !this.controller.get('loadedFromTableSettings')) {
+        return
+      }
+      this.get('refreshAfterFilters').perform()
+    },
+
+    // take the extra parameters
+    runAdvancedSearch (extraParams) {
+      if (this.controller.get('page') !== 1) {
+        this.controller.set('page', 1)
+      }
+      this.controller.set('extraParams', extraParams)
+      this.controller.set('filterParams', {})
+      this.refresh()
+    },
+
+    // general function to open a record from a paginated list
+    // will use transitionTo by default
+    open (record) {
+      const controller = this.controller
+      // my save function
+      this.controller.transitionToRoute(controller.get('modelName'), record)
+    },
+
+    changePage (page) {
+      this.controller.set('page', page)
+      this.refresh()
+    },
+
+    /**
+     *
+     * @param perPage
+     */
+    changePerPage (perPage) {
+      this.controller.set('perPage', perPage)
+      this.refresh()
+    },
+
+    /**
+     *
+     * @param property
+     * @param direction
+     */
+    changeSort (property, direction) {
+      let order = (direction === 'desc') ? '-' : ''
+      this.controller.set('sortProperty', property)
+      this.controller.set('sortDirection', direction)
+      this.controller.set('sortField', `${order}${property}`)
+    },
+
+    /**
+     *
+     * @param subject
+     */
+    selectRow (row, checked) {
+      row.get('content').set('isSelected', checked)
+      if (checked) {
+        this.get('controller.selectedRows').pushObject(row.get('content'))
+      } else {
+        this.get('controller.selectedRows').removeObject(row.get('content'))
+      }
+    },
+    selectAll (checked) {
+      this.get('controller.tableData').setEach('isSelected', checked)
+      if (checked) {
+        this.set('controller.selectedRows', this.get('controller.tableData').toArray())
+      } else {
+        this.set('controller.selectedRows', [])
+      }
+    },
+
+    didTransition () {
+      this.controller.set('selectedRows', [])
+    }
+  },
+
+  // extend to pass route values onto controller for possible use
+  setupController (controller, model) {
+    this._super(controller, model)
+
+    // pass route properties on to controller
+    controller.set('modelName', this.modelName)
+    controller.set('controllerName', this.controllerName)
+    controller.set('currentRoute', this.get('currentRoute'))
+    controller.configureFilterObservers()
+    controller.set('routeKey', this.router.generate(this.get('currentRoute'), ...this.getCurrentRouteParams()))
+    controller.get('loadTableData').perform()
+  },
+
+  /**
+   * @method
+   * @public
+   *
+   *
+   * This method needs to be overrided if the current route
+   * requires params to be generated
+   */
+  getCurrentRouteParams () {
+    return []
+  },
+
+  /**
+   * when a user goes to another case to show the same table
+   * it should reset the data because controllers are singleton
+   * so if the new case doesn't have a table-setting record
+   * it will start in a pristine state
+   */
+  resetController (controller, isExiting, transition) {
+    this._super(...arguments)
+    if (isExiting) {
+      Ember.Logger.info('exiting controller')
+      this.controller.set('loadedFromTableSettings', false)
+
+      this.resetControllerFields(controller, '')
+    }
+  },
+
+  resetControllerFields (controller, sortField) {
+    controller.set('page', 1)
+    controller.set('perPage', 50)
+    controller.set('quickSearch', '')
+    controller.set('sortField', sortField)
+    controller.set('extraParams', {})
+    controller.set('filterParams', {})
+    controller.get('columns').forEach((col) => {
+      col.setProperties({
+        filterValue: null,
+        showFilter: false,
+        advFilterOperator: undefined,
+        advFilterValue: undefined,
+        advFilterValue2: undefined
+      })
+    })
+    controller.get('additionalColumnsForFilter').forEach((col) => {
+      col.setProperties({
+        filterValue: null,
+        showFilter: false,
+        advFilterOperator: undefined,
+        advFilterValue: undefined,
+        advFilterValue2: undefined
+      })
+    })
+  },
+  /**
+   * checks if user is going out of the route to re-init loadedFromTableSettings
+   */
+  willTransition (transition) {
+    const target = transition.targetName
+    if (target !== this.get('currentRoute')) {
+      Ember.Logger.info('Reset loadedFromTableSettings in willTransition')
+      this.controller.set('loadedFromTableSettings', false)
+    }
+  }
+})
